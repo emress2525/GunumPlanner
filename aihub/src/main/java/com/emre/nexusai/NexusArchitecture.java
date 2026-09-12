89c7aafe1c16e8ece69b5a6991b05922dca82dda
emress2525 <kzltrk97@gmail.com>
2026-09-12 18:59:31 +0300

    feat: declare no-login architecture

---
+++ aihub/src/main/java/com/emre/nexusai/NexusArchitecture.java
@@ -0,0 +1,9 @@
+package com.emre.nexusai;
+
+public final class NexusArchitecture {
+    public static final boolean USES_PROVIDER_WEBVIEW = false;
+    public static final boolean REQUIRES_PROVIDER_LOGIN = false;
+    public static final boolean USES_ANDROID_TTS = true;
+
+    private NexusArchitecture() { }
+}
